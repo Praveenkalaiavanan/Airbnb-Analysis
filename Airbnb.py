7916d73f3cd65916b4a2172ccd5a65e935568755
Praveen K <151647417+Praveenkalaiavanan@users.noreply.github.com>
2024-02-10 12:41:15 +0530

    Airbnb.py

--- Airbnb.py
+++ Airbnb.py
@@ -172,7 +172,6 @@ cur.execute('''create table if not exists Main_table (Id int primary key, Name t
        calculated_host_listing_count int, neighbourhood text, No_of_reviews int,
        Review_scores float, Amenities text, Host_id int, Host_name text, Street text,
        Country text, Country_code text, Location_type text, Longitude float, Latitude float)''') 
-
 sql='''insert ignore into Main_table(Id,Name,Description,Property_type,Room_type,Bed_type,Min_nights,Max_nights,Cancellation_policy,Accomodates,Total_bedrooms,Total_beds,
 Availability_365,Price,calculated_host_listing_count,neighbourhood,No_of_reviews,Review_scores,Amenities,Host_id,Host_name,Street,Country,Country_code,
 Location_type,Longitude,Latitude)values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
@@ -189,61 +188,70 @@ for i in range(0,len(df)):
     myconnection.commit()
 
 
-# In[ ]:
+#sql connection
+myconnection=pymysql.connect(host="127.0.0.1",user="root",passwd="Praveen@1234")
+myconnection=pymysql.connect(host="127.0.0.1",user="root",passwd="Praveen@1234",database="airbnb")
+cur=myconnection.cursor()
+
+# Set page configuration
+st.set_page_config(page_title="Airbnb Analysis")
+
+#streamlit 
+SELECT=option_menu(
+    menu_title=None,
+    options=["Home","Explore Data","Steps Done In The Project"],
+    icons=["house","bar-chart","pen"],
+    default_index=2,
+    orientation="horizontal",
+    styles={"container":{"padding":"0!important","background-color":"white","size":"cover","width":"100%"},
+            "icon":{"color":"black","font-size":"20px"},
+            "nav-link":{"font-size":"20px","test-align":"center","margin":"-2px","--hover-color":"#6F36AD"},
+            "nav-link-selected":{"background-color":"#6F36ADj"}})
 
 
+if SELECT=="Home":
+    image1 = Image.open(r"C:\Users\TAMIL TS\project\.venv\Biz.jpg")
+    st.sidebar.image(image1, use_column_width=True)   
+    st.write('''Airbnb is an American San Francisco-based company operating an online marketplace for short- and long-term homestays 
+             and experiences. The company acts as a broker and charges a commission from each booking. The company was founded 
+             in 2008 by Brian Chesky, Nathan Blecharczyk, and Joe Gebbia. Airbnb is a shortened version of its original name,
+               AirBedandBreakfast.com.Airbnb is the most well-known company for short-term housing rentals''')
+    st.write(" ### Key features and aspects of Airbnb include:")
+    st.write('''* Hosts and Guests: Airbnb provides a platform for hosts to list their properties and for guests to find and book 
+             accommodations. Hosts can rent out their entire homes, private rooms, or shared spaces for short-term stays. ''')
+    st.write('''*  Diverse Accommodations: Airbnb offers a wide range of accommodation options, including apartments,
+              houses, villas, cabins, treehouses, and even unique or unconventional spaces''')
+    st.write('''* Global Reach: Airbnb operates in countries around the world, making it a global marketplace for both 
+             hosts and guests.''')
+    st.write('''* Reviews and Ratings: Both hosts and guests can leave reviews and ratings after a stay,
+              contributing to a reputation system that helps ensure trust and transparency within the community. ''')
+    st.write('''* Hosts are responsible for setting house rules, pricing, and managing their listings. ''')
+    st.write('''* Online Booking and Payments: Airbnb facilitates the booking process through its online platform.
+              Guests can search for available properties, communicate with hosts, and make reservations. ''')
+    
 
+if SELECT=="Steps Done In The Project":
+    st.sidebar.write(" ## Technologies Used:")
+    st.sidebar.write("* Python")
+    st.sidebar.write("* MongoDB atlas")
+    st.sidebar.write("* Exploratory Data Analysis")
+    st.sidebar.write("* MySQL")
+    st.sidebar.write("* PowerBI")
+    st.sidebar.write("* Streamlit")
+    st.write("1.Establish a MongoDB connection, retrieve the Airbnb dataset")
+    st.divider()
+    st.write("2.Converting extracted data into DataFrame and then to csv file")
+    st.divider()
+    st.write("3.Clean and prepare the dataset, addressing missing values, duplicates, and data type conversions for accurate analysis")
+    st.divider()
+    st.write("4.Converting cleaned data to CSV file")
+    st.divider()
+    st.write("5.Using the extracted data for visualisation using powerBI and Streamlit")
 
-
-# In[39]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+if SELECT=="Explore Data":
+    st.warning('''For the past projects i have made visualisation using streamlit. So for my skill development i have used PowerBI for visualization in this project.
+             since i am using free version of powerBI i am unable to Publish it .Hence i decided to access it from my system''')
+    
 
 
 
